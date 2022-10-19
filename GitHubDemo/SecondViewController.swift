69714420652850c4b2281e1193c21e8621a0d086
Taras <yogesh.bhatt@galaxyweblinks.in>
2022-10-19 18:08:15 +0530

    Change background color of secondViewcontroller to red

--- GitHubDemo/SecondViewController.swift
+++ GitHubDemo/SecondViewController.swift
@@ -14,6 +14,7 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         label.text = passingText
+        view.backgroundColor = .red
  
         // Do any additional setup after loading the view.
     }

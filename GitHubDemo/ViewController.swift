c8f5c109d8928599fb2ebeea90b837453da11e13
Taras <yogesh.bhatt@galaxyweblinks.in>
2022-10-19 15:03:24 +0530

    Added New functionality to check the number eight in my arrayList

--- GitHubDemo/ViewController.swift
+++ GitHubDemo/ViewController.swift
@@ -11,9 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello world")
+        print(array(para: [5,0,9,0,3,7,2,0]))
     }
-
-
+    func array(para:[Int]) -> Bool {
+        var newArray:[Int] = []
+        for item in 0..<para.count {
+            if para[item] == 8 {
+                newArray.append(8)
+            }
+        }
+        if newArray.count > 1 {
+            return true
+        }
+        return false
+    }
 }
+
+
 
